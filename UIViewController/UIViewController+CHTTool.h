3677c3f8a21f4a61d6670485f223f1644480d227
cht <chen_ht89@163.com>
2016-12-14 11:13:52 +0800

    add UIViewController+CHTTool

---
+++ UIViewController/UIViewController+CHTTool.h
@@ -0,0 +1,21 @@
+//
+//  UIViewController+CHTTool.h
+//  CHTGithub
+//
+//  Created by cht on 16/12/14.
+//  Copyright © 2016年 cht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (CHTTool)
+
+
+/**
+ 获取当前的ViewController
+
+ @return 当前的ViewController
+ */
++ (UIViewController *) currentViewController;
+
+@end
